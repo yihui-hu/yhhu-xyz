e3374d638933e2afb6b7f844ddccad51c1fb6cf5
Yihui <yyihui.hu@gmail.com>
2022-05-01 02:25:44 -0400

    more accurate word count

--- js/readTimeEstimate.js
+++ js/readTimeEstimate.js
@@ -6,7 +6,7 @@ function readTimeEstimate() {
     // hard-coded reading speed -- adults = ~220-350wpm
     const wpm = 200;
     // get total no. of words by splitting the text by spaces
-    const words = text.split(' ').length;
+    const words = text.split(' ').length - 8;
     const minutes = Math.ceil(words / wpm);
     document.getElementById("time").innerText = `${minutes} minute read \u00A0 | \u00A0 ${words} words`;
 }
