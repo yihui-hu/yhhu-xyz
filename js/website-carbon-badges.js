fc29699bcc4235b5d25b0136ae97b87a25136c5b
Yihui <yyihui.hu@gmail.com>
2022-05-10 17:05:51 -0400

    measures carbon usage on main page (i.e. yhhu.xyz)

--- js/website-carbon-badges.js
+++ js/website-carbon-badges.js
@@ -1,5 +1,5 @@
 const wcID = e => document.getElementById(e),
-    wcU = encodeURIComponent(window.location.href),
+    wcU = "yhhu.xyz",
     newRequest = function(e = !0) {
         fetch("https://api.websitecarbon.com/b?url=" + wcU).then((function(e) {
             if (!e.ok) throw Error(e);
