7e74a57769859cd893889e712096235e4c7eec09
Yihui <yyihui.hu@gmail.com>
2022-05-10 17:06:40 -0400

    js file for light / dark mode

---
+++ js/lightdarkmode.js
@@ -0,0 +1,21 @@
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.documentElement.setAttribute("data-theme", "light");
+
+    // Get our button switcher
+    var themeSwitcher = document.getElementById("theme-switcher");
+
+    // When our button gets clicked
+    themeSwitcher.onclick = function () {
+        // Get the current selected theme, on the first run
+        // it should be `light`
+        var currentTheme = document.documentElement.getAttribute("data-theme");
+
+        // Switch between `dark` and `light`
+        var switchToTheme = currentTheme === "dark" ? "light" : "dark"
+
+        // Set our currenet theme to the new one
+        document.documentElement.setAttribute("data-theme", switchToTheme);
+    }
+
+});
