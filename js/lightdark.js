e12fce1d9162cddab46aff5bb6ca13fc41188189
Yihui <yyihui.hu@gmail.com>
2022-05-10 21:26:24 -0400

    fixed errors and made light/dark toggle global

--- js/lightdark.js
+++ js/lightdark.js
@@ -1,23 +1,28 @@
 const themeBtn = document.querySelector('.theme');
 
-function getCurrentTheme(){
+function getCurrentTheme() {
   let theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-  localStorage.getItem('yhhu.theme') ? theme = 
-  localStorage.getItem('yhhu.theme') : null;
+  localStorage.getItem('yhhu.theme') ? theme =
+    localStorage.getItem('yhhu.theme') : null;
   return theme;
 }
 
-function loadTheme(theme){
+function loadTheme(theme) {
   const root = document.querySelector(':root');
+  if (theme === 'light') {
+    themeBtn.innerHTML = `☼&#xFE0E;`;
+  } else {
+    themeBtn.innerHTML = `☾&#xFE0E;`;
+  }
   root.setAttribute('color-scheme', `${theme}`);
 }
 
 themeBtn.addEventListener('click', () => {
   let theme = getCurrentTheme();
   if (theme === 'dark') {
-      theme = 'light';
+    theme = 'light';
   } else {
-      theme = 'dark';
+    theme = 'dark';
   }
   localStorage.setItem('yhhu.theme', `${theme}`);
   loadTheme(theme);
